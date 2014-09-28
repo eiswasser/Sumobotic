ca9a984581abc53a1a2ad66364dab9b87660956c
Marco Bürgi <marco.buergi@stud.hslu.ch>
2014-09-28 16:20:13 +0200

    Platform.h und Platform.c created and supported with doxygen code as knowable

---
+++ INTRO_HANDLE/Common/Platform.c
@@ -0,0 +1,35 @@
+/**
+ * \file
+ * \brief Platform Interface.
+ * \author Marco Bürgi, Pascal Giger
+ *
+ * In this part the modules from the platform are initializing
+ * This includes the initializing of the platform and defined all macros, which
+ * are going to use in the connection with the different projects
+ */
+
+/*! \brief implements the defined interface
+ */
+#include "Platform.h"
+
+/*! \brief implements the necessary interface if LEDs are in use
+ */
+#if PL_USE_LED
+	#include "LED.h"
+#endif
+
+/*! \brief implements the initial method if LEDs are in use
+ */
+void PL_Init(void) {
+#if PL_HAS_LED
+  LED_Init();
+#endif
+}
+
+/*! \brief implements the de-initial method if LEDs are in use
+ */
+void PL_Deinit(void) {
+#if PL_HAS_LED
+  LED_Deinit();
+#endif
+}
---
+++ INTRO_HANDLE/Common/Platform.h
@@ -0,0 +1,56 @@
+/**
+ * \file
+ * \brief Platform Interface.
+ * \author Marco Bürgi, Pascal Giger
+ *
+ * In this part the platform interface is implementing
+ * This includes the initializing of the platform and defining all macros, which
+ * are going to use in the connection with the different projects
+ */
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+#include "PE_Types.h" /* for common Processor Expert types used throughout the project*/
+#include "PE_Error.h" /* global error constants */
+#include <stddef.h>   /* for NULL */
+
+/*! \brief List of the different boards which are in use for the project.
+ *
+ * This gives the opportunity to chose afterwards one of the boards in the codeline.
+ * The name is written as plattform_board_is_xx - PL_BOARD_IS_xx
+ * The compiler option from the projects defines the following macros.
+ */
+#define PL_IS_FRDM	(defined(PL_BOARD_IS_FRDM))
+#define PL_IS_ROBO	(defined(PL_BOARD_IS_ROBO))
+
+/*! \brief The following macros select the support of the LEDs in the project.
+ *
+ * So it gives the opportunity to switch on or off the LEDs in the project.
+ */
+#define PL_USE_LED (1)
+
+/*! \brief Connect the amount of LEDs with the specific board
+ *
+ * In case of which board is used this macro implements the number of LEDs that are
+ * on the board
+ * The FRDM board has 3 LEDs (RGB)
+ * The ROBO board has 2 LEDs
+ */
+#if PL_IS_FRDM
+	#define PL_NOf_LED	(3)
+#elif PL_IS_ROBO
+	#define PL_NOF_LED	(2)
+#else
+	#error "unknown board configuration"
+#endif
+
+/*! \brief Platform initialization
+ */
+void PL_Init(void);
+
+/*! \brief Platform de-initialization
+ */
+void PL_Deinit(void);
+
+#endif
